1d3ffc7875ff060b84152bee905bc692b3d6590e
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2020-11-21 21:01:20 +0100

    Fix tests

--- src/app/global/global.component.spec.ts
+++ src/app/global/global.component.spec.ts
@@ -1,10 +1,13 @@
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { By } from '@angular/platform-browser';
+import { StatsWizEntityHistory } from '../models/stats-wiz';
 import { StatsService } from '../stats.service';
-
+import { SummaryComponent } from '../summary/summary.component';
 import { GlobalComponent } from './global.component';
 
-describe('SummaryComponent', () => {
+describe('GlobalComponent', () => {
   let component: GlobalComponent;
   let fixture: ComponentFixture<GlobalComponent>;
 
--- src/app/summary/summary.component.spec.ts
+++ src/app/summary/summary.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { StatsWizEntityHistory } from '../models/stats-wiz';
 
 import { SummaryComponent } from './summary.component';
 
@@ -8,14 +9,27 @@ describe('SummaryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SummaryComponent ]
-    })
-    .compileComponents();
+      declarations: [SummaryComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SummaryComponent);
     component = fixture.componentInstance;
+    component.summary = new StatsWizEntityHistory({
+      entityName: 'Test Entity',
+      lastUpdate: '1/1/2020',
+      dayone: [
+        {
+          date: '1/5/2020',
+        } as any,
+      ],
+      lastWeek: [
+        {
+          date: '1/5/2020',
+        } as any,
+      ],
+    });
     fixture.detectChanges();
   });
 
